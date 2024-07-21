397a024a4c8d06395a67dd07b1469210cfc410a6
Rajeev Gupta <rajeev.gupta@pb.com>
2024-07-21 17:35:05 +0530

    NonExistingIncrementalNumber in existing array

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,4 +1,5 @@
 public class Solution {
+
     public static void main(String[] args) {
         Solution obj = new Solution();
         System.out.println(obj);
---
+++ src/main/java/Solution_NonExistingIncrementalNumber.java
@@ -0,0 +1,51 @@
+public class Solution_NonExistingIncrementalNumber {
+    public static void main(String[] args) {
+        Solution_NonExistingIncrementalNumber obj = new Solution_NonExistingIncrementalNumber();
+
+        int[] A = new int[] {1, 3, 6, 4, 1, 2};
+        System.out.println(obj.solution(A));
+    }
+
+    public int solution(int[] A) {
+        // Implement your solution here
+        int min = 1;
+        if(A.length == 0) {
+            return 1;
+        }
+
+        int max = max(A) +1;
+        boolean matched = false;
+        for(int i = 1; i< max; i++) {
+            matched = false;
+            for(int a : A) {
+                if(i == a) {
+                    matched = true;
+                }
+            }
+
+            if(!matched) {
+                min = i;
+                break;
+            }
+        }
+
+        if(matched) {
+            min = max;
+        }
+
+
+
+        return min;
+    }
+
+    private int max(int[] a) {
+        int max = a[0];
+        for(int i = 1; i< a.length ; i++) {
+            if(max < a[i]) {
+                max = a[i];
+            }
+        }
+        return max;
+    }
+
+}
