b7a9ae5666761256bc1e53e658d0d3e5f0a70f92
Rajeev Gupta <rajeev.gupta@pb.com>
2024-07-29 11:10:06 +0530

    adding examples

--- src/main/java/java8/StreamsClient.java
+++ src/main/java/java8/StreamsClient.java
@@ -2,6 +2,7 @@ package java8;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -21,7 +22,25 @@ public class StreamsClient {
         //Arrays.stream(obj.fetchDistinctNumbers(new int[]{1, 2, 3, 1, 2})).forEach(System.out:: println);
 
         // take out duplicate numbers from an Array
-        Arrays.stream(obj.fetchDuplicateNumbers(new int[]{1, 2, 3, 1, 2})).forEach(System.out:: println);
+        //Arrays.stream(obj.fetchDuplicateNumbers(new int[]{1, 2, 3, 1, 2})).forEach(System.out:: println);
+
+        // Number start with
+        //Arrays.stream(obj.numberStartWith(new int[]{2, 222, 234, 567, 890, 432, 236, 211, 22}, 2)).forEach(System
+        // .out:: println);
+
+        // Max and Min numbers
+        //int max = Arrays.stream(new int[]{1,3,10,11,5,-1,0,23}).max().getAsInt();
+        int max =
+                Arrays.stream(new int[]{1,3,10,11,5,-1,0,23}).sorted().skip(new int[]{1,3,10,11,5,-1,0,23}.length -1).findFirst().getAsInt();
+        System.out.println("Max: "+ max);
+        //int min = Arrays.stream(new int[]{1,3,10,11,5,-1,0,23}).min().getAsInt();
+        //int min = Arrays.stream(new int[]{1,3,10,11,5,-1,0,23}).sorted().findFirst().getAsInt();
+        int min = Arrays.stream(new int[]{1,3,10,11,5,-1,0,23}).sorted().limit(1).findFirst().getAsInt();
+        System.out.println("Min: "+ min);
+    }
+
+    private int[] numberStartWith(int[] A, int startsWith) {
+        return Arrays.stream(A).mapToObj(a -> String.valueOf(a)).filter(a -> a.startsWith(""+startsWith)).mapToInt(a -> Integer.parseInt(a)).toArray();
     }
 
     private int[] fetchDistinctNumbers(int[] A) {
